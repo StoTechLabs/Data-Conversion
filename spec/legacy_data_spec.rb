4985635c80812e2a77f3b9a28a9ceade0ea50d75
Joshua Storey <jstorey@Joshuas-MacBook-Pro.local>
2014-08-06 07:27:42 -0700

    Create spec to test multiple key value pairs. Spec passes.

--- spec/legacy_data_spec.rb
+++ spec/legacy_data_spec.rb
@@ -5,4 +5,8 @@ describe('data_convert') do
  it 'should change the element of an array to equal its key value in the hash' do
  	data_convert({1 => ["a"]}).should(eq({"a" => 1}))
  end
+
+ it 'should change multiple keys and values and change them into an array' do
+ 	data_convert({1 => ["a"], 2 => ["b"]}).should(eq({"a" => 1, "b" => 2}))
+ end
 end
